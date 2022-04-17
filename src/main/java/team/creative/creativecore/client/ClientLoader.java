090f5ff020e885f932d05ea9e9474ca2409b67de
CreativeMD <Creative-Multimedia-Design@gmx.de>
2022-04-17 22:33:40 +0200

    Some formatting changes

--- src/main/java/team/creative/creativecore/client/ClientLoader.java
+++ src/main/java/team/creative/creativecore/client/ClientLoader.java
@@ -1,29 +1,13 @@
 package team.creative.creativecore.client;
 
 import com.mojang.brigadier.CommandDispatcher;
+
 import net.fabricmc.api.ClientModInitializer;
 
 public interface ClientLoader extends ClientModInitializer {
+    
     @Override
     void onInitializeClient();
-
-    /**
-     * Code should implement like this:
-     *
-     *     public <T> void registerClientCommands(CommandDispatcher<T> dispatcher) {
-     *         dispatcher.register(LiteralArgumentBuilder.<T>literal("ambient-debug").executes(x -> {
-     *             TICK_HANDLER.showDebugInfo = !TICK_HANDLER.showDebugInfo;
-     *             return Command.SINGLE_SUCCESS;
-     *         }));
-     *         dispatcher.register(LiteralArgumentBuilder.<T>literal("ambient-reload").executes(x -> {
-     *             AmbientSounds.reload();
-     *             return Command.SINGLE_SUCCESS;
-     *         }));
-     *     }
-     * @param dispatcher The command dispatcher. Templated.
-     * @param <T> The template type.
-     */
-    default <T> void registerClientCommands(CommandDispatcher<T> dispatcher) {
-
-    }
+    
+    default <T> void registerClientCommands(CommandDispatcher<T> dispatcher) {}
 }
